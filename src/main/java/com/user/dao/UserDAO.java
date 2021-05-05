f46efcd69f08b7bcf34096a8a5b4fbcd63d64f29
K R BHARGAV <bhargavkr2000@gmail.com>
2021-05-05 12:47:49 +0530

    Update UserDAO.java

--- src/main/java/com/user/dao/UserDAO.java
+++ src/main/java/com/user/dao/UserDAO.java
@@ -13,13 +13,6 @@ import java.util.List;
 import com.user.model.*;
 
 
-/**
- * AbstractDAO.java This DAO class provides CRUD database operations for the
- * table users in the database.
- * 
- * @author Ramesh Fadatare
- *
- */
 public class UserDAO {
 	private String jdbcURL = "jdbc:mysql://localhost:3306/demo?characterEncoding=latin1";
 	private String jdbcUsername = "root";
@@ -39,10 +32,10 @@ public class UserDAO {
 			Class.forName("com.mysql.jdbc.Driver");
 			connection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
 		}
 		return connection;
@@ -53,18 +46,15 @@ public class UserDAO {
 
 	public List<User> selectAllUsers() {
 
-		// using try-with-resources to avoid closing resources (boiler plate code)
+		
 		List<User> users = new ArrayList<>();
-		// Step 1: Establishing a Connection
+		
 		try (Connection connection = getConnection();
 
-				// Step 2:Create a statement using connection object
 			PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_USERS);) {
 			System.out.println(preparedStatement);
-			// Step 3: Execute the query or update query
 			ResultSet rs = preparedStatement.executeQuery();
 
-			// Step 4: Process the ResultSet object.
 			while (rs.next()) {
 				int id = rs.getInt("id");
 				String name = rs.getString("name");
